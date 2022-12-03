4234e491c78cb5137a1ce10ad137dbe1f4bb4912
lanmoyan <1728559878@qq.com>
2022-12-03 23:57:30 +0800

    Site updated: 2022-12-03 23:57:29

---
+++ js/NavigationBar.js
@@ -0,0 +1,8 @@
+"" === GLOBAL_CONFIG_SITE.title.replace("懒陌颜", "") ? document.getElementById("page-name-text").style.display = "none" : document.querySelector("#page-name-text>span").innerHTML = document.title.split(" | 懒陌颜")[0];
+
+var $percent = document.querySelector("#nav #hotkey #top-button a.site-page i");
+$percent && window.addEventListener("scroll", (function () {
+    let e = document.body.scrollHeight || document.documentElement.scrollHeight,
+        t = window.innerHeight || document.documentElement.clientHeight;
+    $percent.dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (e - t) * 100).toFixed(0)
+}));
